5e02dd941246917df0d63df8462c804adaafccac
Andrew Cholakian <andrew@andrewvc.com>
2015-06-25 14:55:44 -0500

    Add extra documentation for old plugin users Ignore .idea and *~ emacs files Improve v2 upgrade docs, bump gem version
    Fixes #8

--- lib/logstash/outputs/sns.rb
+++ lib/logstash/outputs/sns.rb
@@ -23,6 +23,12 @@ require "logstash/plugin_mixins/aws_config"
 #  with the @message truncated so that the length of the JSON fits in
 #  `32768` bytes.
 #
+# ## Upgrading to 2.0.0
+#
+# This plugin used to have a `format` option for controlling the encoding of messages prior to being sent to SNS.
+# This plugin now uses the logstash standard https://www.elastic.co/guide/en/logstash/current/configuration.html#codec[codec] option for encoding instead.
+# If you want the same 'plain' format as the v0/1 codec (`format => "plain"`) use `codec => "s3_plain"`.
+#
 class LogStash::Outputs::Sns < LogStash::Outputs::Base
   include LogStash::PluginMixins::AwsConfig::V2
 
