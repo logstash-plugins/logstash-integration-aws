45b71ae5a8d3b323626fe83aac4c63f995c76536
Matt Hoey <matt.hoey@okta.com>
2018-06-01 13:50:54 -0700

    PR Feedback. I felt this was the cleanest approach
    Fixes #30

--- lib/logstash/outputs/sqs.rb
+++ lib/logstash/outputs/sqs.rb
@@ -87,7 +87,8 @@ class LogStash::Outputs::SQS < LogStash::Outputs::Base
   # queue, not the URL or ARN.
   config :queue, :validate => :string, :required => true
 
-  # Account ID of the AWS account which owns the queue
+  # Account ID of the AWS account which owns the queue. Note IAM permissions
+  # need to be configured on both accounts to function.
   config :queue_owner_aws_account_id, :validate => :string, :required => false
 
   public
@@ -101,13 +102,12 @@ class LogStash::Outputs::SQS < LogStash::Outputs::Base
    end
 
     begin
-      @logger.debug('Connecting to SQS queue', :queue => @queue, :region => region, :queue_owner_aws_account_id => @queue_owner_aws_account_id)
-      @queue_url = if @queue_owner_aws_account_id
-        @sqs.get_queue_url(:queue_name => @queue, :queue_owner_aws_account_id => @queue_owner_aws_account_id)[:queue_url]
-      else
-        @sqs.get_queue_url(:queue_name => @queue)[:queue_url]
-      end
-      @logger.info('Connected to SQS queue successfully', :queue => @queue, :region => region, :queue_owner_aws_account_id => @queue_owner_aws_account_id)
+      params = { queue_name: @queue }
+      params[:queue_owner_aws_account_id] = @queue_owner_aws_account_id if @queue_owner_aws_account_id
+
+      @logger.debug('Connecting to SQS queue', params.merge(region: region))
+      @queue_url = @sqs.get_queue_url(params)[:queue_url]
+      @logger.info('Connected to SQS queue successfully', params.merge(region: region))
     rescue Aws::SQS::Errors::ServiceError => e
       @logger.error('Failed to connect to SQS', :error => e)
       raise LogStash::ConfigurationError, 'Verify the SQS queue name and your credentials'
