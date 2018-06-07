56e364b1a164d7000099295eb1da50fef2d9a213
Rob Bavey <rob.bavey@elastic.co>
2018-06-07 13:49:00 -0400

    Add Registry entry for input-sqs

--- lib/registry.rb
+++ lib/registry.rb
@@ -1,6 +1,8 @@
 require "logstash/plugins/registry"
 require "logstash/inputs/s3"
+require "logstash/inputs/sqs"
 require "logstash/outputs/s3"
 
 LogStash::PLUGIN_REGISTRY.add(:input, "s3", LogStash::Inputs::S3)
+LogStash::PLUGIN_REGISTRY.add(:input, "sqs", LogStash::Inputs::SQS)
 LogStash::PLUGIN_REGISTRY.add(:output, "s3", LogStash::Outputs::S3)
